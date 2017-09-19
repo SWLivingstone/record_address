f7f65b17ff76689ef1c62349c87c247ca06ef1c1
SWLivingstone <scottwlivingstone@gmail.com>
2017-09-18 20:37:35 -0700

    Checkpoint 4

--- bloc_record/lib/bloc_record/selection.rb
+++ bloc_record/lib/bloc_record/selection.rb
@@ -123,6 +123,75 @@
      self.find_by(attribute.to_sym, args[0])
    end
 
+   def where(*args)
+     if args.count > 1
+       expression = args.shift
+       params = args
+     else
+       case args.first
+       when String
+         expression = args.first
+       when Hash
+         expression_hash = BlocRecord::Utility.convert_keys(args.first)
+         expression = expression_hash.map {|key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}"}.join(" and ")
+       end
+     end
+
+     sql = <<-SQL
+       SELECT #{columns.join ","} FROM #{table}
+       WHERE #{expression};
+     SQL
+
+     rows = connection.execute(sql, params)
+     rows_to_array(rows)
+   end
+
+   def order(*args)
+     if args.count > 1
+       order = args.join(",")
+     else
+       order = args.first.to_s
+     end
+
+     rows = connection.execute <<-SQL
+       SELECT * FROM #{table}
+       ORDER BY #{order};
+     SQL
+     rows_to_array(rows)
+   end
+
+   def join(*args)
+     if args.count > 1
+       joins = args.map { |arg| "INNER JOIN #{arg} ON #{arg}.#{table}_id = #{table}.id"}.join(" ")
+       rows = connection.execute <<-SQL
+         SELECT * FROM #{table} #{joins}
+       SQL
+     else
+       case args.first
+       when String
+         rows = connection.execute <<-SQL
+           SELECT * FROM #{table} #{BlocRecord::Utility.sql_strings(args.first)};
+         SQL
+       when Symbol
+         rows = connection.execute <<-SQL
+           SELECT * FROM #{table}
+           INNER JOIN #{args.first} ON #{args.first}.#{table}_id = #{table}.id
+         SQL
+       when Hash
+         column1 = args.keys.to_s.gsub!(/[^0-9A-Za-z]/, '')
+         column2 = args.values.to_s.gsub!(/[^0-9A-Za-z]/, '')
+         rows = connection.execute <<-SQL
+           SELECT * FROM #{table}
+           INNER JOIN #{column1} ON #{column1}.#{table}_id = #{table}.id
+           INNER JOIN #{column2} ON #{column2}.#{column1}_id = #{column1}.id
+         SQL
+       end
+     end
+
+     rows_to_array(rows)
+   end
+
+
    private
 
    def not_valid_int(*id)
